cda2018815a8d31c6d27f45db533b1ebc4ee415f
samihda <samihda@users.noreply.github.com>
2016-03-27 18:37:07 +0200

    form validation

--- src/components/form/form.template.js
+++ src/components/form/form.template.js
@@ -1,28 +1,38 @@
 const template = `<section class="col-sm-4">
 					<form novalidate class="form-horizontal" name="searchForm" ng-submit="form.submit()">
-						<div class="form-group">
+						<div class="form-group" ng-class="{'has-error': searchForm.origin.$dirty && searchForm.origin.$invalid}">
 							<label for="inputOrigin" class="col-sm-4 control-label">From</label>
 							<div class="col-sm-8">
 								<input id="inputOrigin"
+										name="origin"
 										class="form-control"
 										type="text"
 										placeholder="try “LAX”" 
+										aria-describedby="inputOriginHelp"
 										ng-model="form.model.origin"
+										ng-pattern="form.pattern.airport"
 										uib-typeahead="airport for airport in form.searchAirport($viewValue) | limitTo:10"
+										required
 								>
+								<span id="inputOriginHelp" class="help-block" ng-show="searchForm.origin.$dirty && searchForm.origin.$invalid">Please select an airport</span>
 							</div>
 						</div>
 
-						<div class="form-group">
+						<div class="form-group" ng-class="{'has-error': searchForm.destination.$dirty && searchForm.destination.$invalid}">
 							<label for="inputDestination" class="col-sm-4 control-label">To</label>
 							<div class="col-sm-8">
 								<input id="inputDestination"
+										name="destination"
 										class="form-control"
 										type="text"
 										placeholder="try “JFK”" 
+										aria-describedby="inputDestinationHelp"
 										ng-model="form.model.destination"
+										ng-pattern="form.pattern.airport"
 										uib-typeahead="airport for airport in form.searchAirport($viewValue) | limitTo:10"
+										required
 								>
+								<span id="inputDestinationHelp" class="help-block" ng-show="searchForm.destination.$dirty && searchForm.destination.$invalid">Please select an airport</span>
 							</div>
 						</div>
 
@@ -54,18 +64,22 @@ const template = `<section class="col-sm-4">
 							</div>
 						</div>
 						
-						<div class="form-group" ng-show="options">
+						<div class="form-group" ng-class="{'has-error': searchForm.maxPrice.$invalid}" ng-show="options">
 							<label for="inputMaxPrice" class="col-sm-4 control-label">Budget</label>
 							<div class="col-sm-8">
 								<div class="input-group">
 									<span class="input-group-addon">€</span>
 									<input id="inputMaxPrice"
+											name="maxPrice"
 											class="form-control" 
 											type="text" 
+											aria-describedby="inputMaxPriceHelp"
 											ng-model="form.model.maxPrice" 
 											ng-pattern="form.pattern.budget" 
-											placeholder="e.g. “1000”">
+											placeholder="e.g. “1000”"
+									>
 								</div>
+								<span id="inputMaxPriceHelp" class="help-block" ng-show="searchForm.maxPrice.$invalid">Enter a valid number</span>
 							</div>
 						</div>
 
